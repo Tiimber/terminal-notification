9e40d2f3f4ed8c70e711531346b615dde0cf1766
Robbin Tapper <robbin@samlingar.com>
2014-06-13 23:32:52 +0200

    2.0: ZIP Packages support almost finished

--- notifier.py
+++ notifier.py
@@ -22,7 +22,7 @@ try:
     # For Python 3.0 and later
     from urllib.request import urlopen
     from urllib.request import HTTPError
-    from urlfetch import fetch
+    from urllib.request import fetch
 except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import urlopen
